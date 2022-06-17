a96b239cc8cf8968a410138a3b366cf731c477ac
Jandy Castillo <jandy.castillo@tecsup.edu.pe>
2022-06-16 22:29:39 -0300

    Add: index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+import card from './components/card.js';
+import spinner from './components/spinner.js';
+
+const $ = (selector) => document.querySelector(selector);
+
+const dollarApiUrl =
+	'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
+
+const getApiData = async (url) => {
+	const response = await fetch(url);
+	const data = await response.json();
+    return data
+};
+
+const main = $('#main-container');
+const isLoading = $('#is-loading');
+isLoading.innerHTML = spinner();
+let cards = '';
+
+(async () => {
+	const data = await getApiData(dollarApiUrl);
+	for (const result of data) {
+		const { compra, nombre, variacion, venta } = result.casa;
+        if (nombre === 'Dolar') continue
+        if (nombre === 'Argentina') continue
+        if (nombre === 'Dolar Soja') continue
+
+		cards += card({
+			name: nombre,
+			buyPrice: compra,
+			sellPrice: venta,
+			variation: variacion,
+		});
+	}
+	main.innerHTML = cards;
+    isLoading.innerHTML = ''
+})();
